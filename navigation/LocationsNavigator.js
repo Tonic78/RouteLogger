f7935e017562a1cee19b6b9e25f19dda779ccf1c
Tonic78 <guymertz@gmaiL.com>
2020-04-17 20:40:30 +0200

    create stacknavigator

---
+++ navigation/LocationsNavigator.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import AddLocationPage from "../pages/AddLocationPage";
+import LocationDetailPage from "../pages/LocationDetailPage";
+import LocationsPage from "../pages/LocationsPage";
+import MapPage from "../pages/MapPage";
+
+const Stack = createStackNavigator();
+
+export default function LocationsNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Locations" component={LocationsPage} />
+        <Stack.Screen name="LocationDetail" component={LocationDetailPage} />
+        <Stack.Screen name="AddLocation" component={AddLocationPage} />
+        <Stack.Screen name="Map" component={MapPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
