f328a23d636351ac0389ed8b2131f2e2b5d47950
Tonic78 <guymertz@gmaiL.com>
2020-04-19 17:13:57 +0200

    setup fetching location data

--- components/PickLocation.js
+++ components/PickLocation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -7,18 +7,61 @@ import {
   Alert,
   StyleSheet,
 } from "react-native";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 export default function PickLocation(props) {
-  const getLocationHandler = () => {};
+  const [pickedLocation, setPickedLocation] = useState();
+  const [isFetching, setIsFetching] = useState(false);
+
+  const verifyPermissions = async () => {
+    const result = await Permissions.askAsync(Permissions.LOCATION);
+    if (result.status !== "granted") {
+      Alert.alert(
+        "Insufficient permissions!",
+        "You need to grant location permissions to use this app.",
+        [{ text: "Okay" }]
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const getLocationHandler = async () => {
+    const hasPermissions = await verifyPermissions();
+    if (!hasPermissions) {
+      return;
+    }
+    try {
+      setIsFetching(true);
+      const loaction = await Location.getCurrentPositionAsync({
+        timeout: 5000,
+      });
+      console.log("what is location", location);
+      setPickedLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    } catch (error) {
+      Alert.alert("Not able to get location", "Pick a location on the map", [
+        { text: "Okay" },
+      ]);
+    }
+    setIsFetching(false);
+  };
 
   return (
     <View style={styles.pickLocation}>
       <View style={styles.mapPreview}>
-        <Text>Ckoose your location!</Text>
+        {isFetching ? (
+          <ActivityIndicator size="large" color="blue" />
+        ) : (
+          <Text>Ckoose your location!</Text>
+        )}
       </View>
       <Button
         title="Get User Location"
-        color="green"
+        color="blue"
         onPress={getLocationHandler}
       />
     </View>
@@ -31,9 +74,11 @@ const styles = StyleSheet.create({
   },
   mapPreview: {
     marginBottom: 10,
-    width: 100%,
+    width: "100%",
     height: 150,
-    borderColor: "green",
-    borderWidth: 1
+    borderColor: "#ccc",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
