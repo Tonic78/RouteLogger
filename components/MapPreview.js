b72da7f6dbfdc03879bc3e0a6966f9e2d33092ea
Tonic78 <guymertz@gmaiL.com>
2020-04-20 15:20:13 +0200

    display user location on map

---
+++ components/MapPreview.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+import { apiKey } from "../config/constants";
+
+export default function MapPreview(props) {
+  let imagePreviewUrl;
+  if (props.location) {
+    imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.latitude},${props.location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.latitude},${props.location.longitude}&key=${apiKey}`;
+  }
+
+  return (
+    <View style={styles.mapPreview}>
+      {props.location ? (
+        <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} />
+      ) : (
+        props.children
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    marginBottom: 10,
+    width: "100%",
+    height: 150,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  mapImage: {
+    width: "100%",
+    height: "100%",
+  },
+});
