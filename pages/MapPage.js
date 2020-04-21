632a0889a08e99221f7e6fdb36790d23423544bb
Tonic78 <guymertz@gmaiL.com>
2020-04-21 21:22:23 +0200

    add marker to select location

--- pages/MapPage.js
+++ pages/MapPage.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 export default function MapPage() {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const mapRegion = {
     latitude: 52.379189,
     longitude: 4.899431,
@@ -10,12 +12,32 @@ export default function MapPage() {
     longitudeDelta: 0.04,
   };
 
+  const selectLocationHandler = (event) => {
+    setSelectedLocation({
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
+    });
+  };
+
+  let markerCoordinates;
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.latitude,
+      longitude: selectedLocation.longitude,
+    };
+  }
+
   return (
     <MapView
       style={styles.map}
       provider={MapView.PROVIDER_GOOGLE}
       region={mapRegion}
-    />
+      onPress={selectLocationHandler}
+    >
+      {markerCoordinates && (
+        <Marker title="Choose location" coordinate={markerCoordinates}></Marker>
+      )}
+    </MapView>
   );
 }
 
