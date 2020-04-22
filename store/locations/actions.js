98f611b03d9292fbfd7c57abb0a30410d4caaea7
Tonic78 <guymertz@gmaiL.com>
2020-04-22 10:57:13 +0200

    setting up expo filesystem

--- store/locations/actions.js
+++ store/locations/actions.js
@@ -1,5 +1,25 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_LOCATION = "ADD_LOCATION";
 
 export const addLocation = (title, image) => {
-  return { type: ADD_LOCATION, locationData: { title: title, image: image } };
+  return async (dispatch) => {
+    const fileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+    dispatch({
+      type: ADD_LOCATION,
+      locationData: { title: title, image: newPath },
+    });
+  };
 };
